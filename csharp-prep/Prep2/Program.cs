cb5205d981645c715540ca167d9c90290298e87c
kim22008@byui.edu <myJeeyoungM78>
2024-01-13 11:43:27 -0700

    Requirments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percent? ");
+        string percent = Console.ReadLine();
+
+        int grade = int.Parse(percent);
+
+        string letter = "";
+
+        if ( grade >= 90)
+        {
+            letter = ("A");
+        }
+        else if ( grade >= 80)
+        {
+            letter = ("B");
+        }
+        else if (grade >= 70)
+        {
+            letter = ("C");
+        }
+        else if (grade >= 60)
+        {
+            letter = ("D");
+        }
+        else if (grade > 60)
+        {
+            letter = ("F");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation you passed");
+        }
+
+        else 
+        {
+            Console.WriteLine("I am sorry better luck next time");
+        }
+ 
     }
 }
